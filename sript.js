190185e5c499774df60cc940662889005ef46c7b
Jeff <145409374+rivedere@users.noreply.github.com>
2023-10-02 07:43:26 -0300

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,36 @@
+var nome = 'Mateus';
+var idade = 28;
+var possuiFacudade = true;
+console.log (nome, idade, possuiFacudade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco);
+
+var comida;
+console.log(comida);
+comida = 'Pizza'
+console.log(comida);
+
+let time = 'Vasco';
+
+console.log(time);
+
+const jogo = 'CS'
+console.log(jogo);
+
+var nome = 'Mateus';
+var idade = 20;
+var comidafav;
+comidafav = 'lasanha';
+var rua, 
+    professor,
+    pais,
+    endereco,
+    rua;
+console.log(nome.idade,comidafav);
+
+
+
