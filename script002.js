03cb4199fca03d1ba605c1b0c991f279e915d73a
Jeff <145409374+rivedere@users.noreply.github.com>
2023-10-04 08:05:11 -0300

    Add files via upload

---
+++ script002.js
@@ -0,0 +1,40 @@
+var nome = 'Andre';
+var idade = 23;
+var simbolo = Symbol();
+console.log(typeof nome);
+
+var nome = 'Andre';
+var sobrenome = 'Lavezzo'
+var nomeCompleto = nome + sobrenome
+
+console.log(nomeCompleto)
+
+var gols = 1000;
+var frase = 'Romario fez' + gols + 'gols';
+console.log(typeof frase);
+
+var ano = 2018;
+var mes = 8;
+console.log(ano + mes);
+
+var frase1 = 'Esse é o \'melhor\'jogo';
+console.log(frase1)
+
+console.log('minecraft');
+
+var gols = 1000;
+var frase2 = 'Romário fez ' + gols + ' gols';
+var frase3 = `Romário fez ${gols} gols`; // Utilizando Template String
+
+var comida = 'Pizza';
+var camisa = '10';
+var idade02 = 20;
+var nome02 = 'Mateus';
+var sobrenome02 = 'Lavezzo';
+var nomeCompleto02 = `${nome02} ${sobrenome02}`;
+var texto = 'it\'s\ tíme';
+console.log(typeof nome02)
+console.log(nomeCompleto02);
+
+
+
