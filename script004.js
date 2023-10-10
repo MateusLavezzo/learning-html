d16b32c2752bf137b8e2f11e71748160f8511f46
Jeff <145409374+rivedere@users.noreply.github.com>
2023-10-10 08:08:10 -0300

    Add files via upload

---
+++ script004.js
@@ -0,0 +1,25 @@
+var possuiGraduacao = true;
+var possuiDoutorado = true; 
+
+if(possuiGraduacao) {
+    console.log('é verdadeiro')
+    var x = 10;
+} else if (possuiDoutorado) {
+    console.log('Possui Doutorado');
+}
+
+var nome = 'Andre';
+
+if(nome) {
+    console.log(nome);
+} else {
+    console.log('Nome não existe');
+}
+
+var x = 10; 
+
+console.log(x === 10)
+
+var y = 'Gatos'
+console.log(y !== 'Gato')
+
