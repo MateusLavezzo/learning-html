47585b7a0e9c2da799fc81117243f1c3211b8126
Jeff <145409374+rivedere@users.noreply.github.com>
2023-10-10 07:22:23 -0300

    Add files via upload

---
+++ script003.js
@@ -0,0 +1,32 @@
+var total = 10 + 5 + 10;
+var divisao = 200 / 5;
+var modulo = 389298656 * 3;
+
+console.log(modulo);
+
+var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+
+console.log(soma1);
+
+var x = 5;
+console.log(x);
+
+var idade = '28';
+var somaidade = 5;
+
+console.log(idade + somaidade);
+
+var expressao1 = 'teste' / 2;
+var expressao2 = 'teste' - 2;
+console.log(expressao1, expressao2);
+
+var soma = +'200' + 50;
+console.log(soma);
+
+var x = 5;
+console.log(++x)
+
+var number = '80' / 2;
+var unidade = 'kg';
+var peso = number + unidade;
+console.log(peso);
